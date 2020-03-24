285d717e48f064b9d1cc13b8ae6dbb2ea3f6b81d
Bruno Martínez <bmartinezg@minsait.com>
2020-03-24 16:24:16 +0100

    verificacion de tamaño de pantalla agregado

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    withScreenVerification () {
+      if (screen.width < 480) {
+        return true
+      } else return false
+    }
+  }
+}
+</script>
 <style lang="scss">
 @import '@/assets/style/main';
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,4 @@
 <template lang="pug">
-  .buenas
-    h1 hola
 </template>
 <script>
 export default {
