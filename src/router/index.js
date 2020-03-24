ffaf2eaae86cf4c6c5aab1e06210c130a46bbe83
Bruno Martínez <bmartinezg@minsait.com>
2020-03-24 14:26:46 +0100

    sistema de diseño insertado ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(VueRouter, ElementUI)
+Vue.use(VueRouter)
 
 const routes = [
 
