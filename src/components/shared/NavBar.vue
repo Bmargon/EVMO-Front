51c551b608f86466b14cd74dbcd3c171136dfb2a
Bruno Martínez <bmartinezg@minsait.com>
2020-03-27 20:02:34 +0100

    navbar done

---
+++ src/components/shared/NavBar.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  section.navbar
+    .navbar__item
+      router-link(:to="'/'")
+        i.el-icon-orange
+    .navbar__item
+      router-link(:to="'/'")
+        i.el-icon-s-ticket
+    .navbar__item
+      router-link(:to="'/'")
+        i.el-icon-user-solid
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+
+<style lang="scss">
+.navbar{
+  width: 100%;
+  height: 80px;
+  @include gradienBlueBackground;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.el-icon-s-ticket,
+.el-icon-user-solid,
+.el-icon-orange {
+  color: white;
+  transform: scale(2);
+}
+</style>
--- src/views/Middleware.vue
+++ src/views/Middleware.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
 section
   router-view
+  nav-bar.navbar
 </template>
 <script>
+import NavBar from '@/components/shared/NavBar'
 export default {
   components: {
+    NavBar
   }
 }
 </script>
+<style lang="scss" scoped>
+.navbar{
+  position: fixed;
+  bottom: 0;
+}
+</style>
